ae4e70d7299e373aee5c1be9a0d3ea4b6c1846c5
Shirley Nicte-ha Heredia Ramos <11470202@itcampeche.edu.mx>
2016-06-06 00:01:06 -0500

    Muestra un conjunto de preguntas y las recorre con un boton sihuiente

---
+++ app/src/main/java/com/nicteheredia/android/model/Pregunta.java
@@ -0,0 +1,32 @@
+package com.nicteheredia.android.model;
+
+/**
+ * Created by Nic on 05/06/2016.
+ */
+public class Pregunta {
+
+    private  int mIdResTexto;
+    private boolean mRespuestaVerdadera;
+
+    public Pregunta(int mIdResTexto, boolean mRespuestaVerdadera) {
+        this.mIdResTexto = mIdResTexto;
+        this.mRespuestaVerdadera = mRespuestaVerdadera;
+    }
+
+    public int getmIdResTexto() {
+        return mIdResTexto;
+    }
+
+    public void setmIdResTexto(int mIdResTexto) {
+        this.mIdResTexto = mIdResTexto;
+    }
+
+    public boolean isVerdadera() {
+        return mRespuestaVerdadera;
+    }
+
+    public void setmRespuestaVerdadera(boolean mRespuestaVerdadera) {
+        this.mRespuestaVerdadera = mRespuestaVerdadera;
+    }
+}
+
